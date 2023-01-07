453847525596c921fb73bbad8e9708577156e197
apwbs <edoardo.marangone@uniroma1.it>
2023-01-07 23:10:55 +0100

    reader public key database interaction

--- architecture/reader_public_key.py
+++ architecture/reader_public_key.py
@@ -3,6 +3,8 @@ from Crypto.PublicKey import RSA
 from hashlib import sha512
 import ipfshttpclient
 import block_int
+import sqlite3
+import io
 
 api = ipfshttpclient.connect('/ip4/127.0.0.1/tcp/5001')
 
@@ -13,28 +15,35 @@ electronics_private_key = config('READER_PRIVATEKEY_SUPPLIER1')
 mechanics_address = config('READER_ADDRESS_SUPPLIER2')
 mechanics_private_key = config('READER_PRIVATEKEY_SUPPLIER2')
 
-reader_address = mechanics_address
-private_key = mechanics_private_key
+reader_address = electronics_address
+private_key = electronics_private_key
+
+# Connection to SQLite3 reader database
+conn = sqlite3.connect('files/reader/reader.db')
+x = conn.cursor()
 
 
 def generate_keys():
     keyPair = RSA.generate(bits=1024)
-    print(f"Public key:  (n={hex(keyPair.n)}, e={hex(keyPair.e)})")
-    print(f"Private key: (n={hex(keyPair.n)}, d={hex(keyPair.d)})")
+    # print(f"Public key:  (n={hex(keyPair.n)}, e={hex(keyPair.e)})")
+    # print(f"Private key: (n={hex(keyPair.n)}, d={hex(keyPair.d)})")
 
-    name_file1 = 'files/keys_readers/handshake_private_key_' + str(reader_address) + '.txt'
-    with open(name_file1, 'w') as ipfs:
-        ipfs.write('reader_address: ' + reader_address + '###')
-        ipfs.write(str(keyPair.n) + '###' + str(keyPair.d))
+    f = io.StringIO()
+    f.write('reader_address: ' + reader_address + '###')
+    f.write(str(keyPair.n) + '###' + str(keyPair.e))
+    f.seek(0)
 
-    name_file = 'files/keys_readers/handshake_public_key_' + str(reader_address) + '.txt'
-    with open(name_file, 'w') as ipfs:
-        ipfs.write('reader_address: ' + reader_address + '###')
-        ipfs.write(str(keyPair.n) + '###' + str(keyPair.e))
-
-    new_file = api.add(name_file)
-    hash_file = new_file['Hash']
+    hash_file = api.add_json(f.read())
     print(f'ipfs hash: {hash_file}')
+
+    # reader address not necessary because each user has one key. Since we use only one 'reader/client' for all the
+    # readers, we need a distinction.
+    x.execute("INSERT OR IGNORE INTO rsa_private_key VALUES (?,?,?)", (reader_address, str(keyPair.n), str(keyPair.d)))
+    conn.commit()
+
+    x.execute("INSERT OR IGNORE INTO rsa_public_key VALUES (?,?,?,?)",
+              (reader_address, hash_file, str(keyPair.n), str(keyPair.e)))
+    conn.commit()
 
     block_int.send_publicKey_readers(reader_address, private_key, hash_file)
 
