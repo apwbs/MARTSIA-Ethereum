057bd571f483e0c9a3ab271a8b2defdb9fdbf6f2
apwbs <edoardo.marangone@uniroma1.it>
2023-02-12 22:08:42 +0100

    small fix

--- implementation/server_monitor_auth4.py
+++ implementation/server_monitor_auth4.py
@@ -70,7 +70,7 @@ def cipher_generated_key(reader_address, process_instance_id, generated_ma_key):
 
 
 def transactions_monitoring():
-    min_round = 8475621
+    min_round = 8481178
     transactions = []
     note = 'generate your part of my key'
     while True:
